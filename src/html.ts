65870a60e3e8a4402690caeb054da974572a29bf
Kat21 <49850194+datkat21@users.noreply.github.com>
2023-09-16 21:33:47 -0500

    Typescriptify

--- src/html.ts
+++ src/html.ts
@@ -1,64 +1,65 @@
-export default class Html {
-  constructor(e) {
+export class Html {
+  elm: HTMLInputElement | HTMLElement;
+  constructor(e: string) {``
     this.elm = document.createElement(e || "div");
   }
-  text(val) {
+  text(val: string) {
     this.elm.innerText = val;
     return this;
   }
-  html(val) {
+  html(val: string) {
     this.elm.innerHTML = val;
     return this;
   }
   cleanup() {
     this.elm.remove();
   }
-  query(selector) {
+  query(selector: any) {
     return this.elm.querySelector(selector);
   }
-  class(...val) {
+  class(...val: string[]) {
     for (let i = 0; i < val.length; i++) {
       this.elm.classList.toggle(val[i]);
     }
     return this;
   }
-  classOn(...val) {
+  classOn(...val: string[]) {
     for (let i = 0; i < val.length; i++) {
       this.elm.classList.add(val[i]);
     }
     return this;
   }
-  classOff(...val) {
+  classOff(...val: string[]) {
     for (let i = 0; i < val.length; i++) {
       this.elm.classList.remove(val[i]);
     }
     return this;
   }
-  style(obj) {
+  style(obj: { [x: string]: string | null }) {
     for (const key of Object.keys(obj)) {
       this.elm.style.setProperty(key, obj[key]);
     }
     return this;
   }
-  on(ev, cb) {
+  on(ev: string, cb: EventListenerOrEventListenerObject) {
     this.elm.addEventListener(ev, cb);
     return this;
   }
-  un(ev, cb) {
+  un(ev: string, cb: EventListenerOrEventListenerObject) {
     this.elm.removeEventListener(ev, cb);
     return this;
   }
-  appendTo(parent) {
+  appendTo(parent: HTMLElement | Html | string) {
     if (parent instanceof HTMLElement) {
       parent.appendChild(this.elm);
     } else if (parent instanceof Html) {
       parent.elm.appendChild(this.elm);
     } else if (typeof parent === "string") {
-      document.querySelector(parent).appendChild(this.elm);
+      document.querySelector(parent)?.appendChild(this.elm);
     }
     return this;
   }
-  append(elem) {
+  append(elem: string | HTMLElement | Html) {
     if (elem instanceof HTMLElement) {
       this.elm.appendChild(elem);
     } else if (elem instanceof Html) {
@@ -66,11 +67,11 @@ export default class Html {
     } else if (typeof elem === "string") {
       const newElem = document.createElement(elem);
       this.elm.appendChild(newElem);
-      return new Html(newElem);
+      return new Html(newElem.tagName);
     }
     return this;
   }
-  appendMany(...elements) {
+  appendMany(...elements: any[]) {
     for (const elem of elements) {
       this.append(elem);
     }
@@ -80,14 +81,15 @@ export default class Html {
     this.elm.innerHTML = "";
     return this;
   }
-  attr(obj) {
+  attr(obj: { [x: string]: string }) {
     for (let key in obj) {
       if (obj[key] !== null) this.elm.setAttribute(key, obj[key]);
       else this.elm.removeAttribute(key);
     }
     return this;
   }
-  val(str) {
-    this.elm.value = str;
+  val(str: any) {
+    var x = this.elm as HTMLInputElement;
+    x.value = str;
   }
 }
