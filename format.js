40f331a0d9d8e00c48880392403c0a8d259085e1
Kat21 <49850194+datkat21@users.noreply.github.com>
2023-12-22 01:30:40 -0600

    Add minified version and license under MIT license.

--- format.js
+++ format.js
@@ -3,16 +3,24 @@ import path from 'node:path';
 import fs from 'node:fs';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import * as esbuild from 'esbuild'
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const compiledJsPath = path.join(__dirname, './dist/html.js');
 const compiledJsContent = fs.readFileSync(compiledJsPath, 'utf-8');
 
-const result = await format(compiledJsContent, {
+const formattedResult = await format(compiledJsContent, {
   parser: 'babel-ts',
   tabWidth: 2,
-  filepath: compiledJsPath
+  filepath: compiledJsPath,
 });
 
-fs.writeFileSync(compiledJsPath, result);
+fs.writeFileSync(compiledJsPath, formattedResult);
+
+await esbuild.build({
+  entryPoints: ['./dist/html.js'],
+  minify: true,
+  outfile: './dist/html.min.js',
+})
+
 console.log(`Formatting (and building) complete. Wrote to ${compiledJsPath}`)
